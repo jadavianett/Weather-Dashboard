2019fc2ac71d16dd516603f1640cf42f2c8bc3e3
Jada Arnett <jada.arnett@gmail.com>
2020-09-14 12:22:12 -0400

    added event listener to form

---
+++ script.js
@@ -0,0 +1,14 @@
+
+$(document).ready(function () {
+    // DOM VARIABLES
+
+    // JS VARIABLES 
+
+    // FUNCTION CALLS 
+
+    // EVENT LISTENERS 
+$("#search-form").on("submit", function (event) {
+    event.preventDefault();
+    console.log("you submitted the form"); 
+});
+});
