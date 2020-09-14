f74750fcf3287cbfd6af7d96f43898e1d9e7d9a5
Jada Arnett <jada.arnett@gmail.com>
2020-09-14 12:36:53 -0400

    added api from open weather and added ajax call to retrieve data from city input

--- script.js
+++ script.js
@@ -1,18 +1,29 @@
-
 $(document).ready(function () {
-    // DOM VARIABLES
+  // DOM VARIABLES
 
-    // JS VARIABLES 
+  // JS VARIABLES
+  var apiKey = "a71c0c9acdfc6c537e04dc620ee2ba12";
 
-    // FUNCTION CALLS 
+  // FUNCTION CALLS
 
-    // EVENT LISTENERS 
-$("#search-form").on("submit", function (event) {
+  // EVENT LISTENERS
+  $("#search-form").on("submit", function (event) {
     event.preventDefault();
-    console.log("you submitted the form"); 
+    console.log("you submitted the form");
     var searchTerm = $("#search-term").val();
     console.log(searchTerm);
-    
-    
-});
+
+    var queryURL =
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+      searchTerm +
+      "&appid=" +
+      apiKey;
+      console.log(queryURL)
+      $.ajax({
+          url: queryURL,
+          method: "GET",
+      }).then(function (response) {
+          console.log(response)
+      })
+  });
 });
